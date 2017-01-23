62ed2f4615668c7ae437bcf788c399ac79254052
miguelangel <miguelangel@Miguel>
2017-01-23 15:39:17 -0500

    prueba de persistencia

--- src/java/co/plan/frontend/controller/ClienteManagedBean.java
+++ src/java/co/plan/frontend/controller/ClienteManagedBean.java
@@ -1,13 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package co.plan.frontend.controller;
 
+import co.plan.backend.model.entities.Cliente;
+import co.plan.backend.persistence.facades.ClienteFacadeLocal;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 
 /**
  *
@@ -16,11 +16,34 @@ import java.io.Serializable;
 @Named(value = "clienteManagedBean")
 @SessionScoped
 public class ClienteManagedBean implements Serializable {
-
-    /**
-     * Creates a new instance of ClienteManagedBean
-     */
+    private Cliente cliente;
+    @Inject private ClienteFacadeLocal cFL;
     public ClienteManagedBean() {
     }
-    
+    @PostConstruct 
+    public void init (){
+        cliente = new Cliente();
+    }
+    public Cliente getObjectByKey(Integer key){
+        return cFL.find(key);
+    }
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public ClienteFacadeLocal getcFL() {
+        return cFL;
+    }
+
+    public void setcFL(ClienteFacadeLocal cFL) {
+        this.cFL = cFL;
+    }
+    //C.R.U.D
+    public void registrar(){
+       cFL.create(cliente);
+    }
 }
