ca1b5fc5e44f915a3e6f826cb00e1d5cec210054
miguelangel <miguelangel@Miguel>
2017-01-25 16:23:17 -0500

    Solución arreglo de la foranéas

---
+++ src/java/co/plan/frontend/cliente/converters/ClienteConverter.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.plan.frontend.cliente.converters;
+
+import co.plan.frontend.logica.AbstractConverter;
+
+/**
+ *
+ * @author miguelangel
+ */
+public class ClienteConverter extends AbstractConverter {
+    public ClienteConverter (){
+        this.namedManagedBean = "vehiculoManagedBean";
+    }
+}
